07d98e705f363f3d6c0d06c0439065c4f34d221e
Alberto.Alonso <jalopas@yahoo.es>
2016-04-16 00:09:45 +0200

    Sube datos a Google.
    Sube datos a google.

--- CODE/Wifi/Proyecto.ino
+++ CODE/Wifi/Proyecto.ino
@@ -4,6 +4,9 @@
     You need to get streamId and privateKey at data.sparkfun.com and paste them
     below. Or just customize this script to talk to other HTTP servers.
 
+https://www.pushingbox.com/
+http://www.instructables.com/id/Post-to-Google-Docs-with-Arduino/
+
 */
 
 #include <ESP8266WiFi.h>
@@ -16,7 +19,7 @@
 #include <SFE_BMP180.h>
 
 // Variable declaration to manage the WIFI networt
-const char* ssid     = "Hotspot";
+const char* WIFI_ssid     = "Hotspot";
 const char* password = "%yatengowifi%";
 // Use WiFiClient class to create TCP connections
 WiFiClient client;
@@ -34,20 +37,21 @@ boolean uploadTemperature = false;
 // ThingSpeak Settings
 char thingSpeakAddress[] = "api.thingspeak.com";
 String writeAPIKey_C = "7J5F3NW8FDLOJDX8";
-// Time interval in milliseconds to update ThingSpeak (number of seconds * 1000 = interval)
-const int updateThingSpeakInterval_C = 16 * 1000;      
+// Google forms Settings
+char Google_form_Address[] = "www.google.es";
+char Google_form_key[] = "1cRJwf9MiV4jKkmAiYpPgx1Uzce4K-DZQ2v0l0ywWUZY"; //Replace with your Key
+//https://docs.google.com/forms/d/1cRJwf9MiV4jKkmAiYpPgx1Uzce4K-DZQ2v0l0ywWUZY/e
+//byte Google_form_Address[] = { 209,85,229,101 }; // Google IP
+//byte Google_form_Address[] = { 216,58,209,163 }; // Google IP Alemania
 
-//// BMP085: Variables para el barometro
-//Barometer BMP085_BAROMETER;
-////BMP085 variables:
-//typedef struct BMP085_type
-//{
-//   float BMP085_T;
-//   float BMP085_Pa;
-//   float BMP085_ATM;
-//   float BMP085_H;
-//};
-//BMP085_type BMP085_data;
+char pushingbox_Address[] = "api.pushingbox.com";
+char pushingbox_ID[] = "vE76BCC4BE2C5A8C";  // THIS IS THE DEVICE ID FROM PUSHINGBOX
+//char pushingbox_ID[] = "v6051E44B14C1CDB";
+char pushingbox_msg[100];
+
+
+// Time interval in milliseconds to update ThingSpeak (number of seconds * 1000 = interval)
+const int updateThingSpeakInterval_C = 16 * 1000;
 
 // BMP180: Variable declaration for the BMP180 barometer.
 //Se declara una instancia de la librería
@@ -76,9 +80,9 @@ void setup() {
    Serial.println();
    Serial.println();
    Serial.print("Connecting to ");
-   Serial.println(ssid);
+   Serial.println(WIFI_ssid);
 
-   WiFi.begin(ssid, password);
+   WiFi.begin(WIFI_ssid, password);
 
    // Repeat until the conexion is established
    while (WiFi.status() != WL_CONNECTED) {
@@ -92,53 +96,11 @@ void setup() {
    Serial.print("IP address: ");
    Serial.println(WiFi.localIP());
 
-//   //Init the sensor
-//   //BMP085 Initialise
-//   BMP085_BAROMETER.init();
-
    //Initialize the sensor BMP180
 //   Start_BMP180_Sensor();
-  pinMode(BUILTIN_LED, OUTPUT); // Onboard LED  
+  pinMode(BUILTIN_LED, OUTPUT); // Onboard LED
 
 }
-
-//int value = 0;
-
-//// Subprogram to read the BMP085 barometer
-//BMP085_type Read_BMP085( )
-//{
-//   //Local variables
-//   float atm;
-//   BMP085_type BMP085_reading;
-//
-//   //Get the temperature, bmp085ReadUT MUST be called first
-//   BMP085_reading.BMP085_T =
-//      BMP085_BAROMETER.bmp085GetTemperature(BMP085_BAROMETER.bmp085ReadUT());
-//
-//   //Get the pressure
-//   BMP085_reading.BMP085_Pa =
-//      BMP085_BAROMETER.bmp085GetPressure(BMP085_BAROMETER.bmp085ReadUP());
-//   BMP085_reading.BMP085_ATM = BMP085_reading.BMP085_Pa / 101325;
-//
-//   //Uncompensated caculation - in Meters
-//   BMP085_reading.BMP085_H =
-//      BMP085_BAROMETER.calcAltitude(BMP085_reading.BMP085_Pa);
-//
-//   // Write results
-//   Serial.print(" BMP085 T: ");
-//   Serial.print(BMP085_reading.BMP085_T, 2); //display 2 decimal places
-//   Serial.print(" ºC  P: ");
-//   Serial.print(BMP085_reading.BMP085_Pa, 0); //whole number only.
-//   Serial.print(" Pa = ");
-//   Serial.print(BMP085_reading.BMP085_ATM, 4); //display 4 decimal places
-//   Serial.print("Atm.  Altitude: ");
-//   Serial.print(BMP085_reading.BMP085_H, 2); //display 2 decimal places
-//   Serial.println(" m");
-//   Serial.println();
-//
-//   return BMP085_reading;
-//
-//};
 
 void Start_BMP180_Sensor() {
 
@@ -173,45 +135,28 @@ void Start_BMP180_Sensor() {
       else Serial.println("Error 2. Temperature is not available.\n");
    }
    else Serial.println("Error 1. Temperature is not initialised.\n");
-
+/*
    // Write results
    Serial.print(" BMP085 T: ");
    Serial.print(BMP180_data.BMP180_T, 2); //display 2 decimal places
-   Serial.print(" ºC  Pres Inicial: ");
+   Serial.print(" C  Pres Inicial: ");
    Serial.print(BMP180_data.BMP180_Initial_P, 0); //whole number only.
    Serial.println();
+*/
 }
 
 // Subprogram to read the BMP085 barometer: Pressure, Temperature and Elevation
 void Read_BMP180( ){
-//   BMP180_data.BMP180_status = 0;
-   Serial.println();
-   Serial.println();
-   Serial.println();
-   Serial.println();
-   Serial.println();
-   Serial.println();
-   Serial.println();
-   Serial.println();
-   Serial.println();
-   Serial.println("*************************************** ");
-   Serial.print("BMP180_data.BMP180_status:    ");
-   Serial.println(BMP180_data.BMP180_status);
-   Serial.println("*************************************** ");
-//   Serial.print("status:    ");
-//   Serial.println(status);
-//   Serial.println("*************************************** ");
-
+ 
    if (BMP180_data.BMP180_status == 0){
       //Barometer sensor status is not available. Initialize the sensor BMP180
       Serial.print("Initialize the sensor BMP180");
       Start_BMP180_Sensor();
 
    }
-   else 
+   else
    {
 
-      Serial.println("Lectura de temperatura ");
       //Se inicia la lectura de temperatura
       BMP180_data.BMP180_status = pressure.startTemperature();
       if (BMP180_data.BMP180_status != 0)
@@ -246,11 +191,82 @@ void Read_BMP180( ){
       }
       else Serial.println("Error iniciando la lectura de temperatura\n");
    }
-   Serial.println("%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
-   Serial.print("BMP180_data.BMP180_status:    ");
-   Serial.println(BMP180_data.BMP180_status);
-   Serial.println("%%%%%%%%%%%%%%%%%%%%%%%%%%%");
 }
+//void updatepushingbox(String)
+//void update_pushingbox(String puData)
+void update_pushingbox()
+{
+   //Connect to wifi
+   Serial.println("");
+   Serial.println("");
+   Serial.print(" Pushingbox: ");
+   Serial.print("Connecting to ");
+   Serial.print(WIFI_ssid);
+   Serial.print(" IP address: ");
+   Serial.println(WiFi.localIP());
+
+   if (client.connect(pushingbox_Address, 80))
+   {
+      Serial.println("connected");
+    //sprintf(pushingbox_msg,"GET /pushingbox?devid=%c&status=%d HTTP/1.1",pushingbox_ID,BMP180_data.BMP180_T);  // NOTE** In this line of code you can see where the temperature value is inserted into the wed address. It follows 'status=' Change that value to whatever you want to post.
+      sprintf(pushingbox_msg,"GET /pushingbox?devid=%s&status=%d HTTP/1.1",pushingbox_ID,BMP180_data.BMP180_T);
+   Serial.print(" BMP180 T: ");
+   Serial.print(BMP180_data.BMP180_T, 2); //display 2 decimal places
+      client.println(pushingbox_msg);
+      client.println("Host: api.pushingbox.com");
+      client.println("Connection: close");
+      client.println();
+
+      Serial.println(pushingbox_msg);
+      Serial.println("Host: api.pushingbox.com");
+      Serial.println("Connection: close");
+      Serial.println();
+ 
+      delay(1000);
+      lastConnectionTime = millis();
+      delay(1000);
+      if (client.connected())
+      {
+         Serial.println("Connecting to Pushingbox ...");
+         Serial.println();
+         failedCounter = 0;
+      }
+      else
+      {
+         failedCounter++;
+         Serial.println("Error 1: Connection to Pushingbox failed (" + String(failedCounter, DEC) + ")");
+         Serial.println();
+      }
+      delay(1000);
+    }
+    else
+    {
+       failedCounter++;
+       Serial.println("Error 2: Connection to Pushingbox Failed (" + String(failedCounter, DEC) + ")");
+       Serial.println();
+       lastConnectionTime = millis();
+    }
+    //***************************************************************************
+    int timeout = millis() + 5000;
+    while (client.available() == 0) {
+       if (timeout - millis() < 0) {
+          Serial.println(">>> Client Timeout !");
+          client.stop();
+          return;
+       }
+    }
+
+    // Read all the lines of the reply from server and print them to Serial
+    while (client.available()) {
+       String line = client.readStringUntil('\r');
+       Serial.print(line);
+    }
+
+    Serial.println("closing connection");
+    Serial.println();
+    Serial.println();
+}
+
 
 //void updateThingSpeak(String tsData)
 void updateThingSpeak(String tsData, String writeAPIKey)
@@ -258,10 +274,11 @@ void updateThingSpeak(String tsData, String writeAPIKey)
    //Connect to wifi
    Serial.println("");
    Serial.println("");
-   Serial.print("IP address: ");
-   Serial.println(WiFi.localIP());
+   Serial.print(" ThingSpeak: ");
    Serial.print("Connecting to ");
-   Serial.println(ssid);
+   Serial.print(WIFI_ssid);
+   Serial.print(" IP address: ");
+   Serial.println(WiFi.localIP());
 
    if (client.connect(thingSpeakAddress, 80))
    {
@@ -278,8 +295,9 @@ void updateThingSpeak(String tsData, String writeAPIKey)
       delay(1000);
       if (client.connected())
       {
-         Serial.println("Connecting to ThingSpeak...");
+ /*        Serial.println("Connecting to ThingSpeak...");
          Serial.println();
+*/
          failedCounter = 0;
       }
       else
@@ -294,6 +312,89 @@ void updateThingSpeak(String tsData, String writeAPIKey)
    {
       failedCounter++;
       Serial.println("Connection to ThingSpeak Failed (" + String(failedCounter, DEC) + ")");
+      Serial.println();
+      lastConnectionTime = millis();
+   }
+   //***************************************************************************
+   int timeout = millis() + 5000;
+   while (client.available() == 0) {
+     if (timeout - millis() < 0) {
+ //      Serial.println(">>> Client Timeout !");
+       client.stop();
+       return;
+     }
+   }
+
+   // Read all the lines of the reply from server and print them to Serial
+   while (client.available()) {
+     String line = client.readStringUntil('\r');
+     Serial.print(line);
+   }
+
+   Serial.println("closing connection");
+   Serial.println();
+   Serial.println();
+
+}
+
+
+//void updateGoogleForms(String)
+void updateGoogleForms(String goData)
+{
+   //Connect to wifi
+   Serial.println("");
+   Serial.println("");
+   Serial.print("IP address: ");
+   Serial.println(WiFi.localIP());
+   Serial.print("Connecting to Google: ");
+   Serial.println(WIFI_ssid);
+
+   if (client.connect(Google_form_Address, 80))
+   {
+      Serial.println("connected");
+
+      client.print("POST /formResponse?formkey=");
+      client.print(Google_form_key);
+      client.println("&ifq HTTP/1.1");
+      client.println("Host: spreadsheets.google.com");
+      client.println("Content-Type: application/x-www-form-urlencoded");
+      client.println("Connection: close");
+      client.print("Content-Length: ");
+      client.println(goData.length());
+      client.println();
+      client.print(goData);
+      client.println();
+/**
+//  Este es el codigo del ejemplo. Es diferente del de thingspeaks.
+  delay(1000);
+  if (!client.connected()) {
+    Serial.println();
+    Serial.println("disconnecting.");
+    client.stop();
+  }
+
+  delay(10000);
+**/
+      lastConnectionTime = millis();
+      delay(1000);
+      if (client.connected())
+      {
+         Serial.println("Connecting to Google Forms ...");
+         Serial.println();
+         failedCounter = 0;
+      }
+      else
+      {
+         failedCounter++;
+         Serial.println("Error 1: Connection to Google Forms failed (" + String(failedCounter, DEC) + ")");
+         Serial.println();
+      }
+      delay(1000);
+   }
+   else
+   {
+      failedCounter++;
+      Serial.println("Error 2: Connection to Google Forms Failed (" + String(failedCounter, DEC) + ")");
       Serial.println();
       lastConnectionTime = millis();
    }
@@ -339,9 +440,7 @@ void loop() {
 
    //Read the BMP180 barometer
    Read_BMP180();
-
    // Write received data
-//   Serial.println(" ////// ");
    Serial.print(" BMP180 T: ");
    Serial.print(BMP180_data.BMP180_T, 2); //display 2 decimal places
    Serial.print(" ºC  P: ");
@@ -349,15 +448,13 @@ void loop() {
    Serial.print(" mBar.  Altitude: ");
    Serial.print(BMP180_data.BMP180_H, 2); //display 2 decimal places
    Serial.println(" m");
-   Serial.println();
-
+//   Serial.println();
    delay(5000);
 
 //******************************************************************************
    // Update ThingSpeak
    if(!client.connected() && (millis() - lastConnectionTime > updateThingSpeakInterval_C))
    {
-//      if (uploadTemperature == true)
       if (uploadTemperature == false)
       {
          // Upload the Temperatures.
@@ -368,95 +465,51 @@ void loop() {
   // Prueba para ver si funciona la subida alimentando con un cargador.
   //***********************
   // initialize digital pin BUILD_LED as an output.
-  digitalWrite(BUILTIN_LED, LOW);   // turn off LED with voltage LOW
-  Serial.println("");
-  Serial.println("******************");
-  Serial.println("** BLINK LOW *****");
-  Serial.println("******************");
-  Serial.println("");
+  digitalWrite(BUILTIN_LED, LOW);   // turn on LED with voltage LOW
+//  Serial.println("");
+//  Serial.println("******************");
+//  Serial.println("** BLINK LOW *****");
+//  Serial.println("******************");
+//  Serial.println("");
   delay(1000);                      // wait one second
-  digitalWrite(BUILTIN_LED, HIGH);  // turn on LED with voltage HIGH
-  Serial.println("");
-  Serial.println("******************");
-  Serial.println("** BLINK HIGH ****");
-  Serial.println("******************");
-  Serial.println("");
+  digitalWrite(BUILTIN_LED, HIGH);  // turn off LED with voltage HIGH
+//  Serial.println("");
+//  Serial.println("******************");
+//  Serial.println("** BLINK HIGH ****");
+//  Serial.println("******************");
+//  Serial.println("");
   delay(1000);                      // wait one second
-//  Serial.println(BUILTIN_LED);
-
-
-
-//         // Reset the variable.
-//         uploadTemperature = false;
-//      }
-//      else
-//      {
-//         // Upload the Monitor data.
-//         updateThingSpeak("field1="+String(contadorciclo,DEC)
-//                        +"&field2="+String(Rlight,DEC)
-//                        +"&field3="+String(sumUV,DEC),writeAPIKeyM);
-//         // Change variable value.
-//         uploadTemperature = true;
+         // Reset the variable.
+         uploadTemperature = true;
+      }
+      else
+      {
+         // Upload the Monitor data.
+        update_pushingbox();   
+        // initialize digital pin BUILD_LED as an output.
+        digitalWrite(BUILTIN_LED, LOW);   // turn on LED with voltage LOW
+        delay(400);                       // wait 300 ms
+        digitalWrite(BUILTIN_LED, HIGH);  // turn off LED with voltage HIGH
+        delay(300);                       // wait 300 ms
+        digitalWrite(BUILTIN_LED, LOW);  // turn on LED with voltage LOW
+        delay(400);                       // wait 300 ms
+        digitalWrite(BUILTIN_LED, HIGH);  // turn off LED with voltage HIGH
+        delay(1000);                      // wait one second
+ /*        updateGoogleForms("entry.0.single="+String(BMP180_data.BMP180_T,DEC)
+                        +"&entry.2.single="+String(BMP180_data.BMP180_Pa,DEC)
+                        +"&submit=Submit");
+ */
+         // Change variable value.
+         uploadTemperature = false;
+ 
       }
    }
    // Check if Arduino Wifi needs to be restarted
    if (failedCounter > 3 )
    {
-      WiFi.begin(ssid, password);
+      WiFi.begin(WIFI_ssid, password);
    }
 
    lastConnected = client.connected();
-//******************************************************************************
-
-//   //Connect to wifi
-//   ++value;
-//   Serial.println("");
-//   Serial.println("");
-//   Serial.print("IP address: ");
-//   Serial.println(WiFi.localIP());
-//   Serial.print("Connecting to ");
-//   Serial.println(host);
-//
-//   // Use WiFiClient class to create TCP connections
-//   WiFiClient client;
-//   const int httpPort = 80;
-//   if (!client.connect(host, httpPort)) {
-//     Serial.println("connection failed");
-//     return;
-//   }
-//
-//   // We now create a URI for the request
-//   String url = "/input/";
-//   url += streamId;
-//   url += "?private_key=";
-//   url += privateKey;
-//   url += "&value=";
-//   url += value;
-//
-//   Serial.print("Requesting URL: ");
-//   Serial.println(url);
-//
-//   // This will send the request to the server
-//   client.print(String("GET ") + url + " HTTP/1.1\r\n" +
-//                "Host: " + host + "\r\n" +
-//                "Connection: close\r\n\r\n");
-//   int timeout = millis() + 5000;
-//   while (client.available() == 0) {
-//     if (timeout - millis() < 0) {
-//       Serial.println(">>> Client Timeout !");
-//       client.stop();
-//       return;
-//     }
-//   }
-//
-//   // Read all the lines of the reply from server and print them to Serial
-//   while (client.available()) {
-//     String line = client.readStringUntil('\r');
-//     Serial.print(line);
-//   }
-//
-//   Serial.println("closing connection");
-//   Serial.println();
-//   Serial.println();
 }
 
